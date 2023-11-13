7a9846212e8b96cd9ab8c7ce9bd6fc8ab574a1f2
devtech5789 <devtech5789@gmail.com>
2023-11-13 14:12:34 +0100

    CRM SIEM first release

---
+++ XufiSIEM/XufiSIEM/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+import requests, json
+
+def index(request):
+    return render(request, 'index.html')
+
+def account(request):
+    return render(request, 'account.html')
+
+def login(request):
+    return render(request, './registration/login.html')
+
+def agent(request):
+    AgentFullList = {}
+    resp = requests.get("http://192.168.1.13:8000/healthcheck/fetch/")
+    print(resp.text)
+    AgentFullList = json.loads(resp.text)
+    context = {"data": AgentFullList}
+    return render(request, 'agentmgmt.html', context)
+
+def scan(request):
+    return render(request, 'scan.html')
+
+def soar(request):
+    return render(request, 'soar.html')
+
+def soaranalysis(request):
+    return render(request, 'soaranalysis.html')
+
+def sbomsecurity(request):
+    return render(request, 'sbomsecurity.html')
+
+def blocklist(request):
+    return render(request, 'blocklist.html')
+
+def agentsettings(request):
+    return render(request, 'agentsettings.html')
